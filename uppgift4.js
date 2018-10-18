5d750f477fd29395cbaaadcef4ee76103a17d2dc
Mohammed Darrh <mmdarrah@hotmail.com>
2018-10-18 13:49:59 +0200

    Final uppdate

--- uppgift4.js
+++ uppgift4.js
@@ -1,11 +1,17 @@
+/* Ett program som beskriver en hiss
+det finns två motoder som man kan använda gotTo och Where()
+så när man lägga till plan numer programet ska reterna vilken plan hissen åker till 
+och på vilken plan hissen äe i.
+*/ 
+
 let hiss = {
 plan : "0",
-goTo : function (par){
+goTo : function (par){ // Denna metod flyttar hissen till det valda numret.
     console.log("Hissen åker till plan " + par + ".");
     this.plan = par;
 },
 
-where : function (){
+where : function (){  // Denna metod vissa vilken plan hissen är i.
     console.log("Hissen är i plan " + this.plan + ".")
 },
 }
